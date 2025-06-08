9d77017d49beb323f0964f478e9c22aaafea6d4e
Artem Folinger <folingerzlive@gmail.com>
2025-06-08 16:59:19 +0300

    321

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'some_default_key_for_development')
 
 DEBUG = False
 
